6e4c7a69d78b73c8a30c9ceb5683e24053a8ead2
abdulsagheer <abdulsagheeras29@gmail.com>
2022-11-17 22:27:32 +0530

    Initialized Note Taking App

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import "react-bootstrap/dist/react-bootstrap.min.js";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import NewNote from "./components/NewNote";
+
+function App() {
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
